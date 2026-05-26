0a273332a555ff72e7c98e3abb5e5c205e444e02
Yashi Bansal <bansalyashi163@gmail.com>
2026-05-26 13:43:04 +0530

    updated backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
-const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+const api = axios.create({
+  baseURL: 'https://job-flow-xi.vercel.app/api',
+  withCredentials: true
 })
 
-export default API
+export default api
